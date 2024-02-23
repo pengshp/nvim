a199aa87f72f8de05562c3ba316161ef9a0e8663
pengshp <pengshp3@outlook.com>
2024-02-24 02:09:48 +0800

    :fire: Improve terinal config.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -69,4 +69,25 @@ return {
       define_autocmd = false,
     },
   },
+
+  -- terminal
+  {
+    "akinsho/toggleterm.nvim",
+    event = "ColorScheme",
+    cmd = "ToggleTerm",
+    keys = {
+      { "<F5>", "<cmd>ToggleTerm<cr>", desc = "ToggleTerm" },
+    },
+    config = function()
+      -- local highlights = require("rose-pine.plugins.toggleterm")
+      require("toggleterm").setup({
+        -- highlights = highlights,
+        open_mapping = [[<F5>]],
+        size = 10,
+        direction = "horizontal",
+        shell = "zsh",
+      })
+    end,
+    enable = true,
+  },
 }
