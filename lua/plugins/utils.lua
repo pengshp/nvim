380eabc6145d39bd7ec89ac72be48be7b94bb18c
pengshp <pengshp3@outlook.com>
2024-09-24 03:03:34 +0800

    :rocket: Optimize some configurations.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -23,19 +23,15 @@ return {
   -- css colorizer.
   {
     "NvChad/nvim-colorizer.lua",
-    ft = { "css", "cfg", "conf", "lua" },
+    ft = { "css", "cfg", "conf", "lua", "rasi" },
     opts = {
       filetypes = { "*" },
       user_default_options = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = false, -- "Name" codes like Blue or blue
         RRGGBBAA = false, -- #RRGGBBAA hex codes
         AARRGGBB = false, -- 0xAARRGGBB hex codes
-        rgb_fn = false, -- CSS rgb() and rgba() functions
-        hsl_fn = false, -- CSS hsl() and hsla() functions
+        names = false, -- "Name" codes
         css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
         -- Available modes for `mode`: foreground, background,  virtualtext
         mode = "background", -- Set the display mode.
         -- Available methods are false / true / "normal" / "lsp" / "both"
@@ -94,5 +90,13 @@ return {
     "soulis-1256/eagle.nvim",
     event = "LspAttach",
     config = true,
+  },
+  {
+    "Exafunction/codeium.nvim",
+    -- enable = false,
+    opts = {
+      detect_proxy = true,
+      enable_chat = false,
+    },
   },
 }
