d8acf8b21520a45fb4b4061fdbd4b343ea4796ea
pengshp <pengshp3@outlook.com>
2024-10-10 05:08:28 +0800

    :fire: Enhance lualine configurations.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "pnx/lualine-lsp-status",
+  },
   event = "ColorScheme",
   opts = {
     options = {
@@ -15,30 +18,9 @@ return {
     sections = {
       lualine_b = {
         { "branch", icon = "" },
-        "diff",
         "diagnostics",
       },
-      lualine_z = {
-        {
-          function()
-            -- return " " .. os.date("%R") .. " "
-            local msg = "No Lsp"
-            local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-            local clients = vim.lsp.get_active_clients()
-            if next(clients) == nil then
-              return msg
-            end
-            for _, client in ipairs(clients) do
-              local filetypes = client.config.filetypes
-              if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return client.name
-              end
-            end
-            return msg
-          end,
-          icon = "",
-        },
-      },
+      lualine_z = { "lsp-status" },
     },
   },
 }
