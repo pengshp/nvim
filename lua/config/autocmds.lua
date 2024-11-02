dd3f59b303841a766d3dbb52dc77a21758b7c83f
pengshp <pengshp3@outlook.com>
2024-11-02 14:11:38 +0800

    fix bug of autocmd.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
+vim.notify = require("notify")
 -- Use LspAttach autocmd to only map the following keys
 -- after the language server attaches to the current buffer.
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -17,21 +18,27 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
   group = ft_lsp_group,
   desc = "Fix the issue where the LSP does not start with docker-compose.",
   callback = function()
-    vim.opt.filetype = "yaml.docker-compose"
+    vim.bo.filetype = "yaml.docker-compose"
   end,
 })
-
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-  pattern = { "*.yml", "*.yaml" },
+-- TODO: fix issue.
+--
+--[[
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "yml", "yaml" },
   group = ft_lsp_group,
   desc = "Fix the issue where the LSP don't start with ansible.",
   callback = function()
-    if vim.fn.findfile("ansible.cfg", vim.fn.getcwd()) then
+    if vim.fn.findfile("ansible.cfg", vim.fn.getcwd(0)) then
       vim.opt.filetype = "yaml.ansible"
+      vim.notify("ansiblels starts successfully", "INFO")
+    else
+      vim.opt.filetype = "yaml"
+      vim.notify("ansible.cfg not found!", "warn")
     end
   end,
 })
-
+--]]
 -- 根据文件类型（filetype)，来自动切换拼写检查功能
 local spell_group = vim.api.nvim_create_augroup("spell_group", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
