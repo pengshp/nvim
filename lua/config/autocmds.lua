db839ff21bec371e32c2f5ae73d0cda5edd8c35f
pengshp <pengshp3@outlook.com>
2024-11-11 20:08:47 +0800

    Update version.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
-vim.notify = require("notify")
 -- Use LspAttach autocmd to only map the following keys
 -- after the language server attaches to the current buffer.
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -55,4 +54,20 @@ vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*", -- disable spellchecking in the embeded terminal
   command = "setlocal nospell",
   group = spell_group,
+})
+
+-- Simple LSP Progress
+vim.api.nvim_create_autocmd("LspProgress", {
+  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+  callback = function(ev)
+    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+    vim.notify(vim.lsp.status(), "info", {
+      id = "lsp_progress",
+      title = "LSP Progress",
+      opts = function(notif)
+        notif.icon = ev.data.params.value.kind == "end" and " "
+          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
+  end,
 })
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -4,12 +4,13 @@ return {
     "CRAG666/code_runner.nvim",
     event = "LspAttach",
     opts = {
-      mode = "toggleterm",
+      mode = "float",
       filetype = {
         lua = "nvim -l",
         markdown = "glow",
         python = "python -u",
         go = "go run",
+        sh = "bash",
       },
     },
     keys = {
@@ -37,36 +38,9 @@ return {
       define_autocmd = false,
     },
   },
-
-  -- terminal
-  {
-    "akinsho/toggleterm.nvim",
-    event = "ColorScheme",
-    cmd = "ToggleTerm",
-    keys = {
-      { "<F5>", "<cmd>ToggleTerm<cr>", desc = "ToggleTerm" },
-    },
-    config = function()
-      -- local highlights = require("rose-pine.plugins.toggleterm")
-      require("toggleterm").setup({
-        -- highlights = highlights,
-        open_mapping = [[<F5>]],
-        size = 10,
-        direction = "horizontal",
-        shell = "zsh",
-      })
-    end,
-    enabled = true,
-  },
   {
     "willothy/wezterm.nvim",
     cmd = "WeztermSpawn",
-    config = true,
-  },
-  -- Show diagnostics and lsp info inside a custom window, following the mouse position
-  {
-    "soulis-1256/eagle.nvim",
-    event = "LspAttach",
     config = true,
   },
   {
